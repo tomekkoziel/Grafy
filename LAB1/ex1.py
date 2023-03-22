9edcc2dddd5b0a5098a620d793c5449d4d4d6bbe
SnoxE <oicmyzs2000@gmail.com>
2023-03-23 00:57:04 +0100

    processing adjacency matrices, based on them: preparing adjacency lists and finding edges, adding nodes and edges to a Networkx nx() Graph, drawing the Graph using nx.draw() (for task 2 it has to be done more/less manually)

---
+++ LAB1/ex1.py
@@ -0,0 +1,48 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def task1():
+    G = nx.Graph()
+    adjmatrix = np.loadtxt('input/adjmatrix.txt', delimiter = ' ', dtype = 'int')
+
+    # add all nodes to the graph
+    nodes = []
+    edges = []
+    for i in range(1, len(adjmatrix) + 1):
+        nodes.append(i)
+
+    G.add_nodes_from(nodes)
+
+    adjlist = []
+    # # print the adjacency matrix
+    # for row in adjmatrix:
+    #     print(row)
+    for i in range(len(adjmatrix)):
+        adjlist.append([i])
+        for j in range(len(adjmatrix[i])):
+            if adjmatrix[i][j] == 1:
+                adjlist[i].append(j + 1)
+
+    # printing the adjacency list
+    print("Adjacency list:")
+    for i in range(len(adjlist)):
+        print(str(adjlist[i][0]) + ": ", end = "")
+        print(*adjlist[i][1:], sep = ", ")
+
+    # add all the edges of the graph
+    for i in range(len(adjmatrix)):
+        for j in range(i, len(adjmatrix[i])):
+            if adjmatrix[i][j] == 1:
+                e = (i + 1, j + 1)
+                edges.append(e)
+
+
+    G.add_edges_from(edges)
+    nx.draw(G, pos = nx.circular_layout(G), with_labels=True, node_color = 'orange')
+    plt.show()
+
+
+task1()
+
