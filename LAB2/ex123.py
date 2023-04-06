6bde37fb693d05b209597c554a5d5235b9698fb5
Karol Hoerner <kahder6@gmail.com>
2023-04-06 18:01:12 +0200

    done task 3

--- LAB2/ex123.py
+++ LAB2/ex123.py
@@ -30,6 +30,7 @@ def draw_graph(matrix):
     nodes = np.zeros(len(matrix),dtype=int)
     keep_going = True
     sum = 0
+    edges = []
     G = nx.Graph()
     for i in range(len(matrix)):
         nodes[i] = i + 1
@@ -48,8 +49,9 @@ def draw_graph(matrix):
         for i in range(1,matrix[0]+1):
             matrix[i] -= 1
             G.add_edge(nodes[0],nodes[i])
+            edges.append((nodes[0],nodes[i]))
         matrix[0] = 0
-    
+    return edges
     nx.draw(G, pos = nx.circular_layout(G), with_labels=True, node_color = '#ffa059', edgecolors = '#ff6d01')
 
 
@@ -115,7 +117,40 @@ def draw_randomize_graph(matrix, n):
     plt.figure("Rand Graph")
     nx.draw(G_rand, pos = nx.circular_layout(G_rand), with_labels=True, node_color = '#ffa059', edgecolors = '#ff6d01')
 
+def components(matrix):
+    G = nx.Graph()
+    nr = 0
+    nodes = np.zeros(len(matrix),dtype=int)
+    comp = np.zeros(len(matrix), dtype=int)
+    edges = []
+    keep_going = True
+    sum = 0
+    for i in range(len(comp)):
+        comp[i] = -1
+    for i in range(len(matrix)):
+        nodes[i] = i + 1
+        G.add_node(i+1)
+    edges = draw_graph(matrix)
+    nodes = sorted(nodes)
+    for node in nodes:
+        if comp[node - 1] == -1:
+            nr += 1
+            comp[node - 1] = nr
+            components_R(nr, node, comp, edges)
+    
+    return comp
 
+def components_R(nr, node, comp, edges):
+    neighbours = []
+    for edge in edges:
+        if(node == edge[0]):
+            neighbours.append(edge[1])
+        elif(node == edge[1]):
+            neighbours.append(edge[0])
+    for neighbour in neighbours:
+        if(comp[neighbour - 1] == -1):
+            comp[neighbour - 1] = nr
+            components_R(nr, neighbour, comp, edges)
 
 def task1(path):
     matrix = np.loadtxt(path, delimiter=" ", dtype=int)
@@ -135,19 +170,39 @@ def task2(path):
         matrix.append(row)
     matrix = matrix[0]
     if(check_if_graphical(matrix)):
-        print(matrix)
-        print(n)
         print("Ciąg jest graficzny")
-        #draw_graph(matrix)
         draw_randomize_graph(matrix, n)
     else:
         print("Ciąg nie jest graficzny")
     plt.show()
 
 
-#task1("./data_ex1.txt")
-task2("./data_ex2.txt")
+def task3(path):
+    matrix = np.loadtxt(path, delimiter=" ", dtype=int)
+    tmpMatrix = matrix
+    if(check_if_graphical(matrix)):
+        print("Ciąg jest graficzny")
+        comp = components(matrix)
+        counter = 1
+        print(comp)
+        while(True):
+            if(max(comp) + 1 == counter):
+                break
+            print(str(counter) + ")", end=" ")
+            for i in range(len(comp)):
+                if(comp[i] == counter):
+                    print(str(i+1),end=" ")
+            print("\n")
+            counter += 1
+            
+    else:
+        print("Ciąg nie jest graficzny")
+    plt.show()
 
+
+#task1("./data_ex1.txt")
+#task2("./data_ex2.txt")
+task3("./data_ex3.txt")
 
 
 
