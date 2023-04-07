63717b68ce876d0b3f03a488b5cbbcf19ab3c040
Karol Hoerner <kahder6@gmail.com>
2023-04-07 16:44:39 +0200

    added comments

--- LAB1/ex3.py
+++ LAB1/ex3.py
@@ -3,39 +3,46 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
+# Funckja tworząca macierz sąsiedztwa grafu o l krawędziach i n wierzchołkach
 def make_rand_graph_edges(n, l):
+    # Sprawdzenie warunków początkowych
     if(l<0 or l > n*(n-1)/2):
         print("Liczba krawędzi musi być z przedziału [0,(n*(n-1))/2]")
         return 0
     if(n<0):
         print("Liczba wierzchołków musi być większa od zera.")
         return 0
-    matrix = np.zeros((n,n),dtype=int)
+    adjmatrix = np.zeros((n,n),dtype=int)
     i = 0
+    # Losowanie krawędzi grafu
     while(i<l):
         row, col = np.random.choice(range(n), size=2, replace=False)
-        if(row > col and matrix[row][col] != 1):
-            matrix[row][col] = 1
-            matrix[col][row] = 1
+        if(row > col and adjmatrix[row][col] != 1):
+            adjmatrix[row][col] = 1
+            adjmatrix[col][row] = 1
             i = i+1
-    print(matrix) 
-    return matrix
+    # print(adjmatrix) 
+    return adjmatrix
 
+# Funckja tworząca macierz sąsiedztwa grafu o n wierzchołkach oraz p prawdopodobieństwie istnienia krawędzi
 def make_rand_graph_propability(n, p):
+    # Sprawdzenie warunków początkowych
     if(p < 0 or p > 1):
         print("Prawdopodobieństwo musi być z przedziału [0,1].")
         return 0
     if(n<0):
         print("Liczba wierzchołków musi być większa od zera.")
         return 0
-    matrix = np.zeros((n,n),dtype=int)
+    adjmatrix = np.zeros((n,n),dtype=int)
+    # Losowanie krawędzi grafu
     for i in range(n):
         for j in range(i+1,n):
-            matrix[i][j] = random.choices([0,1],weights=[1-p,p],k=1)[0]
-            matrix[j][i] = matrix[i][j]
-    print(matrix)
-    return matrix
+            adjmatrix[i][j] = random.choices([0,1],weights=[1-p,p],k=1)[0]
+            adjmatrix[j][i] = adjmatrix[i][j]
+    # print(adjmatrix)
+    return adjmatrix
 
+# Funkcja zwracająca wierzchołki grafu
 def get_nodes_from_adjmatrix(adjmatrix):
     nodes = []
     for i in range(1, len(adjmatrix) + 1):
@@ -43,9 +50,9 @@ def get_nodes_from_adjmatrix(adjmatrix):
 
     return nodes
 
+# Funkcja zwracająca krawędzie grafu
 def get_edges_from_adjmatrix(adjmatrix):
     edges = []
-    
     for i in range(len(adjmatrix)):
         for j in range(i, len(adjmatrix[i])):
             if adjmatrix[i][j] == 1:
@@ -54,19 +61,21 @@ def get_edges_from_adjmatrix(adjmatrix):
 
     return edges
 
-def task3(n,l,p):
-
-    if(l==0):
-        matrix = make_rand_graph_propability(n,p)
-    elif(p==0):
-        matrix = make_rand_graph_edges(n,l)
-    else:
-        return 0
+# Funkcja rysująca graf z macierzy sąsiedztwa
+def draw_graph_from_adjmatrix(adjmatrix):
     G = nx.Graph()
-    G.add_edges_from(get_edges_from_adjmatrix(matrix))
-    G.add_nodes_from(get_nodes_from_adjmatrix(matrix))
-    nx.draw(G,pos=nx.circular_layout(G),with_labels=True, node_color= '#ffa059',node_size = [1000] * len(matrix), edgecolors = '#ff6d01')
+    G.add_edges_from(get_edges_from_adjmatrix(adjmatrix))
+    G.add_nodes_from(get_nodes_from_adjmatrix(adjmatrix))
+    nx.draw(G,pos=nx.circular_layout(G),with_labels=True, node_color= '#ffa059',node_size = [1000] * len(adjmatrix), edgecolors = '#ff6d01')
+
+
+def task3(n, l = 0, p = 0):
+    if(l==0):
+        adjmatrix = make_rand_graph_propability(n,p)
+    elif(p==0):
+        adjmatrix = make_rand_graph_edges(n,l)
+    draw_graph_from_adjmatrix(adjmatrix)
     plt.show()
 
-task3(5,0,0.5)
-task3(5,10,0)
+task3(n = 5, p = 0.1)
+task3(n = 5, l = 9)
