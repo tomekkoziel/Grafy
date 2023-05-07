1c876c4e413d0bb6f84268957f8a1860fe7b8c00
tomekkoziel <tomasz.koziel66@gmail.com>
2023-05-07 12:31:04 +0200

    Added node labels to the drawing function

--- LAB1/ex2.py
+++ LAB1/ex2.py
@@ -20,5 +20,8 @@ def drawCircularGraph(G):
             {(i ): np.array([x_0 + r * np.cos(i * alpha), y_0 + r * np.sin(i * alpha)])})
 
     # Rysowanie
-    nx.draw(G, pos=positions, node_color="#ff6d01", node_size=600)
+    nx.draw(G, pos=positions, node_color="#ff6d01", node_size=600, with_labels=True)
     plt.show()
+    
+    # Testowanie
+    # plt.savefig("test.png")
