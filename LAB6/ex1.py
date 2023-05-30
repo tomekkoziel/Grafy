58590b7de157197e07ffa69e849dc76cd6c9d4ac
SnoxE <oicmyzs2000@gmail.com>
2023-05-30 22:56:00 +0200

    pagerank in both versions

--- LAB6/ex1.py
+++ LAB6/ex1.py
@@ -73,30 +73,6 @@ def create_digraph_from_adjlist(adjacency_list):
     
     return G
 
-def pagerank_m1(digraph):
-    num_nodes = digraph.number_of_nodes()
-    initial_rank = 1 / num_nodes
-    ranks = {node: initial_rank for node in digraph.nodes()}
-
-    # Algorytm PageRank
-    d = 0.15
-    num_iterations = 100000
-    for _ in range(num_iterations):
-        new_ranks = {}
-        for node in digraph.nodes():
-            rank_sum = 0
-            for neighbor in digraph.predecessors(node):
-                num_outlinks = digraph.out_degree(neighbor)
-                if num_outlinks > 0:
-                    rank_sum += ranks[neighbor] / num_outlinks
-
-            rank = (1 - d) / num_nodes + d * rank_sum
-            new_ranks[node] = rank
-
-        ranks = new_ranks
-
-    return ranks
-
 def good_pagerank(G):
     num_nodes = G.number_of_nodes()
     ranks = {node: 1 / num_nodes for node in G.nodes()}
@@ -114,20 +90,24 @@ def good_pagerank(G):
 
     return ranks
 
-# def pagerank(G):
-#     num_nodes = G.number_of_nodes()
-#     initial_rank = 1 / num_nodes
-#     ranks = {node: initial_rank for node in G.nodes()}
+def pagerank_matrix(G, d=0.15, epsilon=1e-8, max_iterations=100):
+    n = G.number_of_nodes()  # liczba wierzchołków
+    p = {node: 1 / n for node in G.nodes()}  # inicjalizacja wektora obsadzeń p0
 
-#     d = 0.15
-#     num_iterations = 10000
-#     for _ in range(num_iterations):
-#         new_ranks = {}
-#         for node in G.nodes:
-#             rank = (1 - d) / num_nodes
+    iteration = 1
+    for _ in range(max_iterations):
+        p_next = {node: 0 for node in G.nodes()}
+        for j in G.nodes():
+            sum_p = sum((1 - d) * p[i] / G.out_degree(i) for i in G.predecessors(j))
+            p_next[j] = sum_p + d / n
+        if sum(abs(p_next[node] - p[node]) for node in G.nodes()) < epsilon:
+            break
+        p = p_next
+        iteration += 1
 
-#             for neighbor in G.predecessors(node):
+    print(iteration)
 
+    return p_next
 
 
 if __name__ == '__main__':
@@ -151,13 +131,13 @@ if __name__ == '__main__':
     print("Nodes:", G.nodes())
     print("Edges:", G.edges())
 
-    pos = nx.spring_layout(G1)
-    nx.draw(G1, pos, node_color = "#ff6d01", node_size = 500)
-    nx.draw_networkx_labels(G1, pos)
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, node_color = "#ff6d01", node_size = 500)
+    nx.draw_networkx_labels(G, pos)
     # nx.draw_networkx_edges(G, pos, arrows=False)
     plt.show()
 
-    ranks_method1 = good_pagerank(G1)
+    ranks_method1 = good_pagerank(G)
 
     sorted_nodes = sorted(ranks_method1, key=ranks_method1.get, reverse=True)
 
@@ -166,9 +146,9 @@ if __name__ == '__main__':
     for node in sorted_nodes:
         print(f"Node: {node}, Rank: {ranks_method1[node]}")
 
-    ranks_method2 = nx.pagerank(G1)
+    ranks_method2 = pagerank_matrix(G)
 
-    sorted_nodes = sorted(ranks_method2, key=ranks_method1.get, reverse=True)
+    sorted_nodes = sorted(ranks_method2, key=ranks_method2.get, reverse=True)
 
     # Wydrukowanie wierzchołków w porządku malejącym
     print("Metoda 2 - Wierzchołki w porządku malejącym:")
